321c5a616073ea3a8e03577b53bcc56322a7c587
CharlesTaylor95 <CharlesTaylor95@gmail.com>
2018-09-29 19:43:45 -0400

    Polynomial input form

--- client/src/components/MathInput.tsx
+++ client/src/components/MathInput.tsx
@@ -1,30 +1,54 @@
 import * as React from 'react';
 import * as katex from 'katex';
 
+
 const MathDisplay = (props: { data: string }) => {
 
   const math = katex.renderToString(props.data);
   return (<p dangerouslySetInnerHTML={ {__html: math} }/>);
 }
 
-export class Input extends React.Component< {}, { text: string }> {
+const defaultState = {text: '', polynomial: [0], katex: ''};
+type State = typeof defaultState;
+
+export class Input extends React.Component< {}, State > {
   constructor(props : {}) {
     super(props);
-    this.state = {text: ''};
+    this.state = defaultState;
     this.updateText = this.updateText.bind(this);
   }
-
+  
   updateText(event: any) {
-    let newText = event.target.value as string;
+    const input = event.target.value as string;
 
-    if (newText.split('')[newText.length - 1] === '-')
-      newText += '----->'
+    const decimal = /(\d*\.?\d* ?)/g;
+    
+    const matches = input.match(decimal)
+      .filter(match => match !== '' && match !== '.');
 
-    this.setState({text: newText});
+    const polynomial = matches
+      .map(match => match === ' ' ? 1 : Number(match));
+
+    const katex = polynomial
+      .map((c, i) => { return {c, i}})
+      .filter(pair => pair.c != 0)
+      .map(({c, i}) => {
+        const coefficient = `${c === 1 ? '' : c}`;
+        const x_term =
+          i === 0 ? '' :
+          i === 1 
+            ? 'x' 
+            : `x^{${i}}`;
+        return `${coefficient}${x_term}`;
+      })
+      .join(" + ");
+
+    const text = matches.join('');
+    this.setState({text, polynomial, katex});
   }
 
   render() {
-    const text = this.state.text;
+    const {text, katex} = this.state;
     return (
       <div 
         style={{
@@ -38,13 +62,13 @@ export class Input extends React.Component< {}, { text: string }> {
           height: '100px',
         }}
         >
-        <MathDisplay data={text}/>
+        <MathDisplay data={katex}/>
         <input 
           style={{
             display: 'inline-block',
           }}
           type='text'
-          value={this.state.text}
+          value={text}
           onChange={this.updateText}/>
       </div>);
   }
